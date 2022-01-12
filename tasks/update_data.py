769313f6892226b068bf90eeb51b9effa4c38fd7
Bob <brstrube@gmail.com>
2022-01-12 06:11:39 +0100

    fix

--- tasks/update_data.py
+++ tasks/update_data.py
@@ -14,8 +14,8 @@ import logging
 def update_symbols_data():
     """ Retrieve and push new data to the database. """
     dir_path = Path('coinStats/data/database.db').absolute()
-    conn = create_connection(dir_path)
-    # conn = sqlite3.connect(dir_path)
+    # conn = create_connection(dir_path)
+    conn = sqlite3.connect(dir_path)
     cur = conn.cursor()
     symbols_data = cur.execute("SELECT id, cmc_id, telegram_url FROM symbols").fetchall()
     cmc_ids = [elem[1] for elem in symbols_data]
